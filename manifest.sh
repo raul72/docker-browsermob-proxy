ef52d5744464c5584d76a36ff7b5322368ab842d
Raul <raulraat@gmail.com>
2018-10-03 18:05:13 +0300

    formatting

--- manifest.sh
+++ manifest.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker manifest create -a "${DOCKER_REPO}:latest" "${DOCKER_REPO}:amd64-latest" "${DOCKER_REPO}:arm32v6-latest" "${DOCKER_REPO}:arm64v8-latest"
 docker manifest annotate "${DOCKER_REPO}:latest" "${DOCKER_REPO}:arm32v6-latest" --os linux --arch arm
 docker manifest annotate "${DOCKER_REPO}:latest" "${DOCKER_REPO}:arm64v8-latest" --os linux --arch arm64 --variant armv8
